0320ba57cce42135c271c8d06323fddeac228805
Dmitry Parfenovich <89684073451@mail.ru>
2022-10-03 22:38:28 +0200

    make correct in translate added

--- src/com/task02/Search.java
+++ src/com/task02/Search.java
@@ -4,7 +4,7 @@ public class Search {
 
     public static void main(String[] args) {
         int[] numbers = new int[]{45, 1, 1, 1, 0, 0, -3, -5, -48};         //Posortowana tablica w porządku rosnącym
-        int x = 0;                                                         //Numer
+        int x = 0;                                                         //Liczba
 
         System.out.println(new Search().search(numbers, x));               //Wdrażanie metody search
     }
@@ -15,8 +15,8 @@ public class Search {
         int midleIndex = (firstIndex + lastIndex) / 2;
 
         /*Jeżeli wartość liczby nie jest większa lub mniejsza niż
-         maksymalne i minimalne wartości tablicy, to liczba nie może
-         tam być i nie jest wykonywane dalsze wyszukiwanie.
+         maksymalna i minimalna wartości tablicy, to liczba nie może
+         znajdować się w tej tablicy i nie jest wykonywane dalsze wyszukiwanie.
          Złożoność obliczeniową algorytmy O(1), pamięciową O(1)*/
 
         if ((x > numbers[firstIndex]) && (x < numbers[lastIndex])) {
